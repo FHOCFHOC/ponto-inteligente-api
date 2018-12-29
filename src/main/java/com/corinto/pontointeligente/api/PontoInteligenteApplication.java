84cbf9ff8d2e1447afd32803fa21a87f0eca8678
FHOCFHOC <backintheusa@gmail.com>
2018-12-28 23:00:41 -0200

    Adicionado ehCache

--- src/main/java/com/corinto/pontointeligente/api/PontoInteligenteApplication.java
+++ src/main/java/com/corinto/pontointeligente/api/PontoInteligenteApplication.java
@@ -1,14 +1,11 @@
 package com.corinto.pontointeligente.api;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude = { 
-	    SecurityAutoConfiguration.class 
-	})
+@EnableCaching
 public class PontoInteligenteApplication {
 
 	public static void main(String[] args) {
